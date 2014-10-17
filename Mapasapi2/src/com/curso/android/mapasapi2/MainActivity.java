128511a4aa99f93ce06e75b9157624ae70b9a05a
Dafydd24 <dafydd24@gmail.com>
2014-10-17 16:17:20 -0500

    Google Maps v2
    Ejercicios con google Maps Android API v2

---
+++ Mapasapi2/src/com/curso/android/mapasapi2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.curso.android.mapasapi2;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
